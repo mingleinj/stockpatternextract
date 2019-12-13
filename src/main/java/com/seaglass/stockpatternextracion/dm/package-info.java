ad85bdc4568ff9a4f7c7a5d4d67235c523363e49
mingl <mingl@DESKTOP-SCEM6VA>
2019-12-12 22:16:53 -0500

    init

---
+++ src/main/java/com/seaglass/stockpatternextracion/dm/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author mingl
+ *
+ */
+package com.seaglass.stockpatternextracion.dm;
---
+++ src/main/java/com/seaglass/stockpatternextraction/datasource/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author mingl
+ *
+ */
+package com.seaglass.stockpatternextraction.datasource;
